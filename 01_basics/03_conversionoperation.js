27c335eb043e8808edef1fc78ae83739b179b069
Mohd Shavan <143516309+aswtmstaw@users.noreply.github.com>
2024-06-30 15:02:08 +0000

    Conversion of Data types

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,22 @@
+let score = "33"
+
+//console.log(typeof score);
+// yaha capt me aur ab score definatly number me convert ho gaya hai. neeche wali line
+//let valueInNumber = Number(score)
+
+//console.log( typeof valueInNumber);
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "shaban" => true
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
