f3203c4052b9c984d2e7dafe78f7574d54570629
Mohd Shavan <143516309+aswtmstaw@users.noreply.github.com>
2024-06-30 17:54:17 +0000

    conversion

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,4 +1,4 @@
-let score = "33"
+//let score = "33"
 
 //console.log(typeof score);
 // yaha capt me aur ab score definatly number me convert ho gaya hai. neeche wali line
@@ -9,14 +9,41 @@ let score = "33"
 // "33abc" => NaN
 //  true => 1; false => 0
 
-let isLoggedIn = 1
-let booleanisLoggedIn = Boolean(isLoggedIn)
+//let isLoggedIn = 1
+//let booleanisLoggedIn = Boolean(isLoggedIn)
 //console.log(booleanisLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "shaban" => true
 
-let stringNumber = String(someNumber)
+//let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+
+
+//--------------operations------------------
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "assalam alaikum"
+let str2 = " shaban"
+
+let str3 = str1 + str2
+console.log(str3);
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
