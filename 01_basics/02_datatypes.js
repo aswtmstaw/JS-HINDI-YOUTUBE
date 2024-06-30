a34fc36c4250adb3c6de11c1fe7d93e53f165099
Mohd Shavan <143516309+aswtmstaw@users.noreply.github.com>
2024-06-30 14:36:48 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as new version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("shaban")
+
+
+let name = "shaban"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined ayega iska output
+console.log(typeof null); // object ayega iska output, aur ye sab interview me bhi pooch lete hain "
